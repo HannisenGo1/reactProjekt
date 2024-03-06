bbe17421c0b0f2da47a1c4d9092f45bc5418c987
HannisGo1 <Hannakarlsson69@gmail.com>
2024-03-06 13:44:14 +0100

    tillagda rader

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,52 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+	<main>
+
+  <h1 class="excisting"> Hollywood watch </h1>
+
+<label for="site-search">Search movie:</label>
+<input type="search" id="site-search" name="q" />
+	
+
+
+<div class="book-flex">
+			<div class="card">
+				<img alt="bild" />
+				<div class="under-bild">
+				<h3>my neighbour totoro</h3>
+				<p> 2021-09-28 </p></div>
+			</div>
+			<div class="card">
+				<img alt="bild" />
+				<div class="under-bild">
+				<h3>The Matrix Resurrections</h3>
+				<p> 2021-12-22 </p> </div>
+			</div>
+			<div class="card">
+				<img alt="bild" />
+				<div class="under-bild"> 
+				<h3>Dune</h3>
+				<p> 2021-10-21 </p></div> 
+			</div>
+			<div class="card">
+				<img alt="bild" />
+				<div class="under-bild"> 
+				<h3>No time to Die</h3>
+				<p> 2021-09-28 </p></div>
+			</div>
+			<div class="card">
+				<img alt="bild" />
+				<div class="under-bild"> 
+				<h3>Spider-man: No way home</h3>
+				<p> 2021-12-17 </p> </div>
+			</div>
+		</div>
+	</main>
   )
 }
+
+	
+
 
 export default App
